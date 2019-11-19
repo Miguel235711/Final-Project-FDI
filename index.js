19fa8b45de8ddfea319bdaca1fe194d30eea6b84
Miguel235711 <a01423629@itesm.mx>
2019-11-19 12:20:02 -0600

    uploading files

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+//import models from './models'
+const assert = require('assert');
+//import models, { connectDb } from './models';
+//const models,{connectDB} = require('./models');
+const app = express();
+//const router = express.Router();
+
+app.set('view engine','ejs');///ejs view engine
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/',(req,res)=>{
+    res.render('test.ejs');
+});
+
+app.listen(3000,()=>{
+    console.log('Server started on port 3000');
+});
